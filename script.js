ceeb3e0b03c3a9bd6efd09c5009e13a8ca413860
Nazanin <126393077+nazaninsaedi@users.noreply.github.com>
2023-08-26 15:21:19 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -62,6 +62,90 @@ test("There is at least one image", () => {
 });
 test("There are at least 500 words on the page", () => {
   expect(getWords.length).toBeGreaterThanOrEqual(500);
+}); var dice, rolls, item, total;
+
+// Describe this function...
+function showtotal() {
+  if (--window.LoopTrap <= 0) throw "Infinite loop.";
+  let element_total = document.getElementById('total');
+  element_total.innerText = rolls.reduce((a, b) => a + b, 0);
+}
+
+// Describe this function...
+function showinfo() {
+  if (--window.LoopTrap <= 0) throw "Infinite loop.";
+  showRolls();
+  showtotal();
+  showResult();
+}
+
+function randomInt(n) {
+  // Return a random number from in [0, n[
+  return Math.floor(Math.random() * n);
+}
+
+function randomMember(arr) {
+  // Return a random member of the array
+  return arr[randomInt(arr.length)]
+}
+
+// Describe this function...
+function showResult() {
+  if (--window.LoopTrap <= 0) throw "Infinite loop.";
+  if (rolls.reduce((a, b) => a + b, 0) == total) {
+    let element_info = document.getElementById('info');
+    element_info.innerText = 'you win';
+  } else if (rolls.reduce((a, b) => a + b, 0) < total) {
+    let element_info2 = document.getElementById('info');
+    element_info2.innerText = 'keep going';
+  } else {
+    let element_info3 = document.getElementById('info');
+    element_info3.innerText = 'you lost';
+  }
+}
+
+// Describe this function...
+function showRolls() {
+  if (--window.LoopTrap <= 0) throw "Infinite loop.";
+  let element_list2 = document.getElementById('list');
+  element_list2.replaceChildren();
+  rolls.forEach((item) => {
+    let element_list3 = document.getElementById('list');
+    let new_li = document.createElement('li');
+    new_li.innerText = item;
+
+    element_list3.appendChild(new_li);
+  });
+}
+
+
+dice = [1, 2, 3, 4, 5, 6];
+rolls = [];
+total = 11;
+showinfo();
+
+
+document.getElementById('button_roll').addEventListener('click', (event) => {
+  rolls.push(randomMember(dice));
+  showinfo();
+
+});
+
+document.getElementById('button_remove').addEventListener('click', (event) => {
+  rolls.pop();
+  showinfo();
+
+});
+
+document.getElementById('button_restart').addEventListener('click', (event) => {
+  rolls = [];
+  let element_list = document.getElementById('list');
+  element_list.replaceChildren();
+  let element_total2 = document.getElementById('total');
+  element_total2.innerText = '0';
+  let element_total3 = document.getElementById('total');
+  element_total3.innerText = 'keep going!';
+
 });
 
 const console = document.getElementById("tests");
